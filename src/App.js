6bcef2e91478e46164d851503a7fbd018fc82cd9
西山 雄大 <nishiyamayudai@team-lab.com>
2020-09-03 17:04:35 +0900

    02 - Basic Queries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
 import { useQuery } from 'react-query'
+import axios from 'axios'
 
 export default function App() {
-  console.log(useQuery)
-  return <div>Hello React Query!</div>
+  const queryInfo = useQuery('pokemon', () =>
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon').then(
+      res => res.data.results
+    )
+  )
+
+  return (
+    <div>
+      {queryInfo.data?.map(result => {
+        return <div key={result.name}>{result.name}</div>
+      })}
+    </div>
+  )
 }
