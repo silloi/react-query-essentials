524b9ed5bcebdc3677681cbb87fb5c672a099f24
西山 雄大 <nishiyamayudai@team-lab.com>
2020-09-10 19:39:24 +0900

    07 - Configuring Query Stale Time

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import { ReactQueryDevtools } from 'react-query-devtools'
 import axios from 'axios'
 
 function Pokemon() {
-  const queryInfo = useQuery('pokemon', async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    return axios
-      .get('https://pokeapi.co/api/v2/pokemon')
-      .then((res) => res.data.results)
-  })
+  const queryInfo = useQuery(
+    'pokemon',
+    async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      return axios
+        .get('https://pokeapi.co/api/v2/pokemon')
+        .then((res) => res.data.results)
+    },
+    {
+      staleTime: Infinity,
+    }
+  )
 
   return queryInfo.isLoading ? (
     'Loading...'
