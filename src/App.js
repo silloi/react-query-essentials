863b85e485269839c5d95ab54b6cbcfa87784eab
西山 雄大 <nishiyamayudai@team-lab.com>
2020-09-14 19:10:31 +0900

    12 - Parallel Queries

--- src/App.js
+++ src/App.js
@@ -7,30 +7,43 @@ import axios from 'axios'
 export default function App() {
   return (
     <div>
-      <Count />
       <Pokemon />
+      <Berries />
       <ReactQueryDevtools />
     </div>
   )
 }
 
-function usePokemon() {
-  return useQuery('pokemons', async () => {
+function Berries() {
+  const queryInfo = useQuery('berries', async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    return axios
+      .get('https://pokeapi.co/api/v2/berry')
+      .then((res) => res.data.results)
+  })
+
+  return queryInfo.isLoading ? (
+    'Loading...'
+  ) : queryInfo.isError ? (
+    queryInfo.error.message
+  ) : (
+    <div>
+      {queryInfo.data?.map((result) => {
+        return <div key={result.name}>{result.name}</div>
+      })}
+      <br />
+      {queryInfo.isFetching ? 'Updating...' : null}
+    </div>
+  )
+}
+
+function Pokemon() {
+  const queryInfo = useQuery('pokemon', async () => {
     await new Promise((resolve) => setTimeout(resolve, 1000))
     return axios
       .get('https://pokeapi.co/api/v2/pokemon')
       .then((res) => res.data.results)
   })
-}
-
-function Count() {
-  const queryInfo = usePokemon()
-
-  return <h3>You are looking at {queryInfo.data?.length} pokemon</h3>
-}
-
-function Pokemon() {
-  const queryInfo = usePokemon()
 
   return queryInfo.isLoading ? (
     'Loading...'
