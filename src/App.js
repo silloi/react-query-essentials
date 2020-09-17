0c1f42ccfb3ab4c118d7035ad471f113f52e9068
西山 雄大 <nishiyamayudai@team-lab.com>
2020-09-17 20:27:49 +0900

    16 - Query Cancallation (not work)

--- src/App.js
+++ src/App.js
@@ -2,31 +2,30 @@ import React from 'react'
 import { useQuery } from 'react-query'
 import { ReactQueryDevtools } from 'react-query-devtools'
 
-import axios from 'axios'
-
-export default function App() {
-  const [pokemon, setPokemon] = React.useState('')
-  return (
-    <div>
-      <input value={pokemon} onChange={(e) => setPokemon(e.target.value)} />
-      <PokemonSearch pokemon={pokemon} />
-      <ReactQueryDevtools />
-    </div>
-  )
-}
-
 function PokemonSearch({ pokemon }) {
   const queryInfo = useQuery(
     ['pokemon', pokemon],
-    async () => {
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-      return axios
-        .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-        .then((res) => res.data)
+    () => {
+      const controller = new AbortController()
+
+      const signal = controller.signal
+
+      const promise = new Promise((resolve) => setTimeout(resolve, 1000))
+        .then(() => {
+          return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, {
+            method: 'get',
+            signal,
+          })
+        })
+        .then((res) => res.json())
+
+      promise.cancel = () => {
+        controller.abort()
+      }
+
+      return promise
     },
     {
-      retry: 2,
-      retryDelay: false,
       enabled: pokemon,
     }
   )
@@ -44,6 +43,17 @@ function PokemonSearch({ pokemon }) {
       )}
       <br />
       {queryInfo.isFetching ? 'Updating...' : null}
+    </div>
+  )
+}
+
+export default function App() {
+  const [pokemon, setPokemon] = React.useState('')
+  return (
+    <div>
+      <input value={pokemon} onChange={(e) => setPokemon(e.target.value)} />
+      <PokemonSearch pokemon={pokemon} />
+      <ReactQueryDevtools />
     </div>
   )
 }
