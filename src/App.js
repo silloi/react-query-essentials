ee5818464af5346169229cb32e175b1dd06750be
西山 雄大 <nishiyamayudai@team-lab.com>
2020-10-20 19:26:40 +0900

    21 - Querying Related Lists and Items

--- src/App.js
+++ src/App.js
@@ -5,37 +5,79 @@ import axios from 'axios'
 
 import existingUser from './existingUser'
 
-const email = 'Sincere@april.biz'
+function Posts({ setPostId }) {
+  const postsQuery = useQuery('posts', async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => res.data)
+  })
 
-function MyPosts() {
-  const userQuery = useQuery(
-    'user',
-    async () => {
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-      return axios
-        .get(`https://jsonplaceholder.typicode.com/users?email=${email}`)
-        .then((res) => res.data[0])
-    },
-    {
-      initialData: existingUser,
-      initialStale: true,
-    }
-  )
-
-  return userQuery.isLoading ? (
-    'Loading user...'
-  ) : (
+  return (
     <div>
-      <pre>{JSON.stringify(userQuery.data, null, 2)}</pre>
-      {userQuery.isFetching ? 'Updating...' : null}
+      <h1>Posts {postsQuery.isFetching ? '...' : null}</h1>
+      <div>
+        {postsQuery.isLoading ? (
+          'Loading posts...'
+        ) : (
+          <ul>
+            {postsQuery.data.map((post) => {
+              return (
+                <li key={post.id}>
+                  <a onClick={() => setPostId(post.id)} href="#">
+                    {post.title}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        )}
+      </div>
+    </div>
+  )
+}
+
+function Post({ postId, setPostId }) {
+  // https://jsonplaceholder.typicode.com/posts/${id}
+
+  const postQuery = useQuery(['post', postId], async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    return axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((res) => res.data)
+  })
+
+  return (
+    <div>
+      <a onClick={() => setPostId(-1)} href="#">
+        Back
+      </a>
+      <br />
+      <br />
+      {postQuery.isLoading ? (
+        'Loading...'
+      ) : (
+        <>
+          {postQuery.data.title}
+          <br />
+          <br />
+          {postQuery.isFetching ? 'Updating...' : null}
+        </>
+      )}
     </div>
   )
 }
 
 export default function App() {
+  const [postId, setPostId] = React.useState(-1)
+
   return (
     <div>
-      <MyPosts />
+      {postId > -1 ? (
+        <Post postId={postId} setPostId={setPostId} />
+      ) : (
+        <Posts setPostId={setPostId} />
+      )}
       <ReactQueryDevtools />
     </div>
   )
