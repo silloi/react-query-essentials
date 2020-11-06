ed6da4098522079f5663d4458f19b470e6a2ef39
西山 雄大 <nishiyamayudai@team-lab.com>
2020-11-06 14:38:30 +0900

    46 - Using React Query with Next.js (Part 1)

--- pages/[postId].js
+++ pages/[postId].js
@@ -1,49 +1,24 @@
 import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useQuery, useMutation, queryCache } from 'react-query'
+import { useQuery } from 'react-query'
 import axios from 'axios'
 
 //
 
-import PostForm from '../components/PostForm'
+const fetchPost = async (id) => {
+  await new Promise((r) => setTimeout(r, 500))
+  return axios
+    .get('https://jsonplaceholder.typicode.com/posts/' + id)
+    .then((res) => res.data)
+}
 
-export default function Post() {
+export default function Post({ post }) {
   const {
     query: { postId },
   } = useRouter()
 
-  const postQuery = useQuery(['post', postId], () =>
-    axios.get(`/api/posts/${postId}`).then((res) => res.data)
-  )
-
-  const [savePost, savePostInfo] = useMutation(
-    (values) =>
-      axios.patch(`/api/posts/${values.id}`, values).then((res) => res.data),
-    {
-      onMutate: (values) => {
-        queryCache.cancelQueries(['post', String(values.id)])
-
-        const oldPost = queryCache.getQueryData(['post', String(values.id)])
-
-        queryCache.setQueryData(['post', String(values.id)], values)
-
-        return () =>
-          queryCache.setQueryData(['post', String(values.id)], oldPost)
-      },
-      onError: (error, values, rollback) => {
-        if (rollback) {
-          rollback()
-        }
-      },
-      onSuccess: (data, values) => {
-        queryCache.setQueryData(['post', String(values.id)], data)
-      },
-      onSettled: (data, error, values) => {
-        queryCache.invalidateQueries(['post', String(values.id)])
-      },
-    }
-  )
+  const postQuery = useQuery(['post', postId], () => fetchPost(postId))
 
   return (
     <>
@@ -51,29 +26,16 @@ export default function Post() {
         <span>Loading...</span>
       ) : (
         <div>
+          <Link href="/">
+            <a>Back</a>
+          </Link>
           <h3>
-            <Link href="/[postId]" as={`/${postQuery.data.id}`}>
-              <a>
-                {postQuery.data.title} {postQuery.isFetching ? '...' : null}
-              </a>
-            </Link>
+            {postQuery.data.title} {postQuery.isFetching ? '...' : null}
           </h3>
           <p>
             <small>Post ID: {postQuery.data.id}</small>
           </p>
-          <PostForm
-            initialValues={postQuery.data}
-            onSubmit={savePost}
-            submitText={
-              savePostInfo.isLoading
-                ? 'Saving...'
-                : savePostInfo.isError
-                ? 'Error!'
-                : savePostInfo.isSuccess
-                ? 'Saved!'
-                : 'Save Post'
-            }
-          />
+          <p>{postQuery.data.body}</p>
         </div>
       )}
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,54 @@
 import React from 'react'
+import Link from 'next/link'
+import { useQuery } from 'react-query'
 import axios from 'axios'
-import { useInfiniteQuery } from 'react-query'
 
-const fetchPosts = (_, page = 0) =>
-  axios
-    .get('/api/posts', {
-      params: {
-        pageOffset: page,
-        pageSize: 10,
-      },
-    })
-    .then((res) => res.data)
+const fetchPosts = async () => {
+  await new Promise((r) => setTimeout(r, 500))
+  return axios
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.data.slice(0, 10))
+}
 
-export default function Posts() {
-  const postsQuery = useInfiniteQuery('posts', fetchPosts, {
-    getFetchMore: (lastPage) => lastPage.nextPageOffset,
+export const getServerSideProps = async () => {
+  const posts = await fetchPosts()
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+export default function Posts({ posts }) {
+  const postsQuery = useQuery('posts', fetchPosts, {
+    initialData: posts,
+    initialStale: true,
   })
 
   return (
-    <div>
-      {postsQuery.isLoading ? (
-        <span>Loading...</span>
-      ) : (
-        <>
-          <h3>Posts {postsQuery.isFetching ? <small>...</small> : null}</h3>
-          <ul>
-            {postsQuery.data.map((page, index) => {
-              return (
-                <React.Fragment key={index}>
-                  {page.items.map((post) => (
-                    <li key={post.id}>{post.title}</li>
-                  ))}
-                </React.Fragment>
-              )
-            })}
-          </ul>
-          <br />
-        </>
-      )}
-      <button
-        onClick={() => postsQuery.fetchMore()}
-        disabled={!postsQuery.canFetchMore}
-      >
-        Fetch More
-      </button>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-    </div>
+    <section>
+      <div>
+        <div>
+          {postsQuery.isLoading ? (
+            <span>Loading...</span>
+          ) : (
+            <>
+              <h3>Posts {postsQuery.isFetching ? <small>...</small> : null}</h3>
+              <ul>
+                {postsQuery.data.map((post) => (
+                  <Link href="/[postId]" as={`/${post.id}`} key={post.id}>
+                    <a>
+                      <li key={post.id}>{post.title}</li>
+                    </a>
+                  </Link>
+                ))}
+              </ul>
+              <br />
+            </>
+          )}
+        </div>
+      </div>
+    </section>
   )
 }
